17089b4de8b90b61559dfd5c1dfd756ace2fe139
oscar <oscarniegu@gmail.com>
2022-10-28 21:21:33 +0200

    primera version

---
+++ zshinstall/zshinstall.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+paru -S --needed git zsh fzf ttf-meslo-nerd-font-powerlevel10k
+
+# instalar oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+#tema porwerlevel10k
+
+"git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+#complementos
+
+"git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions"
+
+"git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+
+"git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete"
+
+"git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting"
+
+"cp .zshrc $HOME/.zshrc"
+
+exec zsh
+
