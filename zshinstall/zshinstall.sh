a1f8b8992fc965abff2a29e9adc475136926e7bf
oscar <oscarniegu@gmail.com>
2022-10-28 22:56:56 +0200

    mal copiado

--- zshinstall/zshinstall.sh
+++ zshinstall/zshinstall.sh
@@ -1,6 +1,6 @@
-! /usr/bin/bash
+#! /usr/bin/bash
 
-paru -S --needed git fzf ttf-meslo-nerd-font-powerlevel10k
+paru -S --needed git zsh fzf ttf-meslo-nerd-font-powerlevel10k
 
 # instalar oh my zsh
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -8,7 +8,7 @@ paru -S --needed git fzf ttf-meslo-nerd-font-powerlevel10k
 
 #tema porwerlevel10k
 
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerl>
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 #complementos
 
@@ -18,10 +18,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/pl>
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 
 cp .zshrc $HOME/.zshrc
 
 exec zsh
-
-
