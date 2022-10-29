a47a2561b084782027948dc8b788eeb636d22a2a
oscar <oscarniegu@gmail.com>
2022-10-29 23:55:13 +0200

    version 0.3

--- zshinstall/zshplugins.sh
+++ zshinstall/zshplugins.sh
@@ -1,20 +1,35 @@
 #! /usr/bin/bash
-
+CUR_USER=$USER
 
 #tema porwerlevel10k
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+
+sed -ier 's/ZSH_THEME=\"\w*\"/ZSH_THEME=\"\powerlevel10k/powerlevel10k"/g' ~/.zshrc
+sed -ier 's/plugins=(git)/plugins=(git sudo fzf zsh-completions zsh-autosuggestions zsh-autocomplete zsh-syntax-highlighting)/g' ~/.zshrc
+echo 'alias zshconfig="nano ~/.zshrc"' >> ~/.zshrc
+echo 'alias ls="lsd"' >> ~/.zshrc
+echo 'alias la="lsd -la"' >> ~/.zshrc
+echo "export EDITOR='nano'" >> ~/.zshrc
+sudo chsh -s /bin/zsh "$CUR_USER"
+
 #complementos
 
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+#git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+#git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-cp .zshrc $HOME/.zshrc
+#git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+#git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+#cp .zshrc $HOME/.zshrc
 
 exec zsh
