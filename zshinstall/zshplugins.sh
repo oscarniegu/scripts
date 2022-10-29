8f295f7ad7fdbcd136da7113195dc3464273f923
oscar <oscarniegu@gmail.com>
2022-10-29 17:33:18 +0200

    version 0.2

---
+++ zshinstall/zshplugins.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+
+#tema porwerlevel10k
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+#complementos
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
+
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+
+cp .zshrc $HOME/.zshrc
+
+exec zsh
